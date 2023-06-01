fe4aba778e210fbc98187d37c44edbee976405f2
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-06-01 23:57:25 +0300

    Set up the project
    This commit sets up the project, using create react app as well as react-router, prettier and eslint-config-prettier. Also, updates to user-event library package to the last version and creates initials files for development.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+
+import App from './components/App';
+
+import './index.css';
+import './App.css';
+import './styles/header/Header.css';
+import './styles/header/Logo.css';
+import './styles/main/Main.css';
+import './styles/main/About.css';
+import './styles/main/CongratulationsPopUp.css';
+import './styles/main/Leaderboard.css';
+import './styles/main/Level.css';
+import './styles/main/Levels.css';
+import './styles/main/Main.css';
+import './styles/main/NotFound.css';
+import './styles/footer/Footer.css';
+import './styles/footer/Github.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
